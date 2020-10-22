fcc73f7a4147321904de607b0fd11bcffd346aa8
sebong <osb4226@gmail.com>
2020-10-22 19:40:57 +0900

    [add] 백준_컨베이어 벨트위의 로봇

--- test.java
+++ test.java
@@ -3,15 +3,13 @@ import java.util.Scanner;
 public class test {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		
-		if(a>b) {
-			System.out.println(">");
-		}else if(a<b) {
-			System.out.println("<");
-		}else {
-			System.out.println("==");
+//		int a = sc.nextInt();
+//		int b = sc.nextInt();
+		int d = -1000;
+		int N = 4;
+		while(d<0) {
+			d += N;
 		}
+		System.out.println(d);
 	}
 }
