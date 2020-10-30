c8ac4719138d58b46c4dbf78ffb4c13fc2ce175e
sebong <osb4226@gmail.com>
2020-10-30 23:29:37 +0900

    [add] 백준_N과M_9

---
+++ 백준_N과M_9.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class 백준_N과M_9 {
+	static int N, M;
+	static int[] arr;
+	static boolean[] visited;
+	static ArrayList<Integer> list;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] str;
+		str = br.readLine().split(" ");
+		N = Integer.parseInt(str[0]);
+		M = Integer.parseInt(str[1]);
+		arr = new int[N];
+		visited = new boolean[N];
+		list = new ArrayList<Integer>();
+		str = br.readLine().split(" ");
+		for (int i = 0; i < str.length; i++) {
+			arr[i] = Integer.parseInt(str[i]);
+		}
+		Arrays.sort(arr);
+		dfs(0);
+
+	}
+
+	private static void dfs(int depth) {
+		if (depth == M) {
+			for (int i = 0; i < list.size(); i++) {
+				System.out.print(list.get(i) + " ");
+			}
+			System.out.println();
+		}
+		int temp = 0;
+		for (int i = 0; i < N; i++) {
+			if (visited[i] || arr[i] == temp)
+				continue;
+			temp = arr[i];
+			visited[i] = true;
+			list.add(arr[i]);
+			dfs(depth + 1);
+			visited[i] = false;
+			list.remove(list.size() - 1);
+		}
+
+	}
+}
